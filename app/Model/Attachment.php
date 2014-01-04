37115b27ed3fd7ce4f275f7dfab11e4bff5a30c2
hoo89 <hoo@me.com>
2014-01-05 02:14:46 +0900

    fix upload file name decision routine

--- app/Model/Attachment.php
+++ app/Model/Attachment.php
@@ -6,25 +6,15 @@ class Attachment extends AppModel {
             array('rule' => 'isUnique','message' => '別のファイル名を指定してください')
             )
         );
-    /*public $actsAs = array(
-        'Upload.Upload' => array(
-            'attachment' => array(
-                'thumbnailSizes' => array(
-                    'xvga' => '1024x768',
-                    'vga' => '640x480',
-                    'thumb' => '80x80',
-                ),
-                'fields' => array(
-                    'dir' => 'dir'
-                )
-            ),
-        ),
-    );*/
+
     public $belongsTo = 'WikiPage';
-    public function beforeSave($options = array()) {
-        $upload_dir = 'files/attachment';
+    public function beforeValidate($options = array()) {
         setlocale(LC_ALL, 'ja_JP.UTF-8');
-        $this->data[$this->alias]['name'] = basename($this->data[$this->alias]['name']);
+        $this->data[$this->alias]['name'] = basename(h($this->data[$this->alias]['name']));
+        return true;
+    }
+    public function beforeSave($options = array()){
+        $upload_dir = 'files/attachment';
         $filename=$this->data[$this->alias]['name'];
         $upload_file = $upload_dir.DS.$filename;
         move_uploaded_file($this->data['Attachment']['attachment']['tmp_name'],$upload_file);
