f5854ffd62309e5dcfa9e37887dcda2d51aa2dea
hoo89 <hoo89@me.com>
2014-01-23 20:12:11 +0900

    fix cacheAction

--- app/Controller/WikiPagesController.php
+++ app/Controller/WikiPagesController.php
@@ -4,9 +4,9 @@ class WikiPagesController extends AppController {
     public $presetVars = true;
     public $helpers = array('Cache');
     public $cacheAction = array(
-        'view/' => '1 week',
-        'index/' => '1 week',
-        'public_index/' => '1 week'
+        'view' => '1 week',
+        'index' => '1 week',
+        'public_index' => '1 week'
     );
     var $paginate = array(
         'limit' => 20,
