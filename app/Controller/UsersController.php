8b28ef2e3233ac944243c4f0f1c8447a884c610a
hoo89 <hoo@me.com>
2014-01-24 14:20:50 +0900

    fix redirect after login

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -28,10 +28,18 @@ class UsersController extends AppController {
         if ($this->request->is('post')) {
             if ($this->Auth->login()) {
                 $this->Session->write('name', $this->Auth->user('username'));
-                $this->redirect('/');
+                $url = $this->Session->read('url');
+                if(preg_match("/\\/user\\/login$/", $url)){
+                    $url='/';
+                }
+                $this->Session->delete('url');
+                $this->redirect($url);
+                
             } else {
                 $this->Session->setFlash(__('ユーザ名かパスワードが間違っています.'));
             }
+        }else{
+            $this->Session->write('url', $this->request->referer(null,true));
         }
     }
 
