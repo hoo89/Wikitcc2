11f623e0058487f69ee89fdab423355d729ad291
hoo89 <hoo@me.com>
2014-02-14 18:26:34 +0900

    add WikiPage revisions table schema

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -63,4 +63,22 @@ class AppSchema extends CakeSchema {
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
 
+	public $wiki_page_revs = array(
+		'version_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
+		'version_created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10),
+		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'body' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'category_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 10),
+		'is_public' => array('type' => 'boolean', 'null' => true, 'default' => '1'),
+		'format' => array('type' => 'integer', 'null' => true, 'default' => '0', 'length' => 10),
+		'attachment_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 10),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'version_id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
 }
