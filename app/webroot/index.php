7bc3477a74626551e685f6aab21856575c4cd459
hoo89 <hoo89@me.com>
2016-04-26 02:46:37 +0900

    Use composer to install CakePHP and Text_Diff

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -62,6 +62,10 @@ if (!defined('APP_DIR')) {
  * /lib/Cake/Console/Templates/skel/webroot/index.php
  */
 //define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
+define(
+    'CAKE_CORE_INCLUDE_PATH',
+    ROOT . DS . APP_DIR . DS . 'Vendor' . DS . 'cakephp' . DS . 'cakephp' . DS . 'lib'
+);
 
 /**
  * Editing below this line should NOT be necessary.
