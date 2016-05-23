7ee0debf9c8eea51ef37839f564c9df14075394a
hoo89 <hoo89@me.com>
2016-05-24 01:55:00 +0900

    Add all test

---
+++ app/Test/Case/allTest.php
@@ -0,0 +1,12 @@
+<?php
+class allTest extends CakeTestSuite {
+
+    public static function suite() {
+        $suite = new CakeTestSuite('All tests');
+        $suite->addTestDirectory(TESTS . 'Case' . DS . 'Controller');
+        $suite->addTestDirectory(TESTS . 'Case' . DS . 'Model');
+        $suite->addTestDirectory(TESTS . 'Case' . DS . 'View');
+        return $suite;
+    }
+
+}
