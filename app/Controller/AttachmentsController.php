dff6e41017f81325fc5da7b01dd37a45f24c8e35
hoo89 <hoo89@me.com>
2016-05-25 08:47:25 +0900

    Refactor AttachmentsController

--- app/Controller/AttachmentsController.php
+++ app/Controller/AttachmentsController.php
@@ -4,45 +4,60 @@
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
- * 
+ *
  * @copyright	Copyright (c) 2014, hoo89
  * @link		https://github.com/hoo89/Wikitcc2
  * @license		MIT License
  */
 
 class AttachmentsController extends AppController{
-	public function add($title=null){
+
+/**
+ * Add Attachment
+ * If posted Attachment, upload and create Attachment, then redirect previous page.
+ *
+ * @param string @title
+ * @return void
+ */
+	public function add($title = null) {
 		if ($this->request->is('post')) {
 			$this->Attachment->create();
 			if ($this->Attachment->save($this->request->data)) {
-				$this->Session->setFlash('アップロードが完了しました.','alert/success');
+				$this->Session->setFlash('アップロードが完了しました.', 'alert/success');
 				return $this->redirect($this->referer());
 			}
-			$this->Session->setFlash('アップロードできませんでした.','alert/danger');
+			$this->Session->setFlash('アップロードできませんでした.', 'alert/danger');
 		}
 
-		$wiki_page_id=null;
+		$wikiPageId = null;
 		if (!empty($title)){
 			$post = $this->Attachment->WikiPage->findByTitle($title);
 			if ($post) {
-				$wiki_page_id = $post['WikiPage']['id'];
-				$this->set('content_title',$post['WikiPage']['title']);
+				$wikiPageId = $post['WikiPage']['id'];
+				$this->set('content_title', $post['WikiPage']['title']);
 			}
 		}
-		$this->set('wiki_page_id',$wiki_page_id);
+		$this->set('wiki_page_id', $wikiPageId);
 	}
 
+/**
+ * Show Attachments
+ * If it called from requestAction(), this return array of Attachments.
+ * If named paramater 'title' given, add condition 'wiki_pages.title = [title] AND attachments.wiki_page_id = wiki_pages.id'
+ *
+ * @return array|void
+ */
 	public function index() {
 		if (!empty($this->request['named']['title'])){
 			$post = $this->Attachment->WikiPage->findByTitle($this->request['named']['title']);
-			$wiki_page_id = 0;
+			$wikiPageId = 0;
 
 			if ($post) {
-				$wiki_page_id = $post['WikiPage']['id'];
-				$this->set('content_title',$post['WikiPage']['title']);
-				$this->set('wiki_page_id',$wiki_page_id);
+				$wikiPageId = $post['WikiPage']['id'];
+				$this->set('content_title', $post['WikiPage']['title']);
+				$this->set('wiki_page_id', $wikiPageId);
 			}
-			$attachments = $this->paginate(array('Attachment.wiki_page_id' => $wiki_page_id));	
+			$attachments = $this->paginate(array('Attachment.wiki_page_id' => $wikiPageId));
 		} else {
 			$attachments = $this->paginate();
 		}
@@ -54,12 +69,17 @@ class AttachmentsController extends AppController{
 		}
 	}
 
+/**
+ * Delete Attachment
+ *
+ * @param string @id
+ */
 	public function delete($id) {
 		if ($this->Attachment->delete($id)){
-			$this->Session->setFlash('ファイルを削除しました','alert/success');
+			$this->Session->setFlash('ファイルを削除しました', 'alert/success');
 		} else {
-			$this->Session->setFlash('ファイルの削除に失敗しました','alert/danger');
+			$this->Session->setFlash('ファイルの削除に失敗しました', 'alert/danger');
 		}
-		$this->redirect($this->referer(null,true));
+		$this->redirect($this->referer(null, true));
 	}
 }
