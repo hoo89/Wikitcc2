d902a3426441b448b8b6887c95e8623be3856f68
hoo89 <hoo89@me.com>
2016-05-25 08:00:37 +0900

    Refactor WikiPage

--- app/Model/WikiPage.php
+++ app/Model/WikiPage.php
@@ -11,6 +11,12 @@
  */
 
 class WikiPage extends AppModel {
+
+/**
+ * バリデーションルール
+ *
+ * @var array
+ */
 	public $validate = array(
 		'title' => array(
 			array('rule' => 'notBlank'),
@@ -19,27 +25,65 @@ class WikiPage extends AppModel {
 		)
 	);
 
+/**
+ * WikiPage belongs to Category
+ *
+ * @var string
+ */
 	public $belongsTo = 'Category';
+
+/**
+ * WikiPage has many Attachments
+ *
+ * @var string
+ */
 	public $hasMany = 'Attachment';
 
-	public function beforeSave($options = array()) {
-		$this->Behaviors->load('Revision',array('limit'=>$this->limit_revisions));
-		return true;
-	}
+/**
+ * RevisionBehaviorが残す履歴の最大数
+ *
+ * @var int
+ */
+	private $limit_revisions = 100;
 
-	public $actsAs = array('Search.Searchable');
+/**
+ * SearchableBehaviorの検索条件指定
+ *
+ * @var array
+ */
 	public $filterArgs = array(
 		'title' => array('type' => 'like'),
 		'body' => array('type' => 'like'),
-		'keyword' => array('type' => 'like', 'field' => array('WikiPage.title', 'WikiPage.body'),'connectorAnd' => ' ', 'connectorOr' => ','),
+		'keyword' => array('type' => 'like', 'field' => array('WikiPage.title', 'WikiPage.body'), 'connectorAnd' => ' ', 'connectorOr' => ','),
 		'is_public' => array('type' => 'value')
 	);
 
-	private $limit_revisions = 100;
+/**
+ * SearchableBehavior
+ *
+ * @var array
+ */
+	public $actsAs = array('Search.Searchable');
 
-	public function getRevision(){
-		if(empty($this->revision)){
-			$this->Behaviors->load('Revision',array('limit'=>$this->limit_revisions));
+/**
+ * 保存前にRevisionBehaviorを動的に読み込む
+ *
+ * @param array $options
+ * @return bool
+ */
+	public function beforeSave($options = array()) {
+		$this->Behaviors->load('Revision', array('limit' => $this->limitRevisions));
+		return true;
+	}
+
+/**
+ * 履歴を管理するモデルを返す
+ *
+ * @return Model WikiPagesRev
+ */
+	public function getRevision() {
+		if (empty($this->revision)) {
+			$this->Behaviors->load('Revision', array('limit' => $this->limitRevisions));
 			$this->revision = $this->ShadowModel;
 		}
 
