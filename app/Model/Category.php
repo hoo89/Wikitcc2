33bb8bfa326413e0e8f4ba38bb611a8fc88a17bc
hoo89 <hoo89@me.com>
2016-05-25 07:50:17 +0900

    Refactor Category

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -11,22 +11,39 @@
  */
 
 class Category extends AppModel {
+
+/**
+ * To use TreeBehavior
+ *
+ * @var array
+ */
 	public $actsAs = array('Tree');
-	public $name = 'Category';
+
+/**
+ * Category has many WikiPages
+ *
+ * @var array
+ */
 	public $hasMany =
 	array(
-		'WikiPage'=>array(
-		   'fields' => array('id','title','category_id','is_public'),
+		'WikiPage' => array(
+		   'fields' => array('id', 'title', 'category_id', 'is_public'),
 		'order' => 'WikiPage.modified DESC'
 		)
 	);
 
-	public function beforeDelete($options = array()){
+/**
+ * カテゴリ削除前に自身と下位カテゴリに属するページのカテゴリをTOPへ変更する
+ *
+ * @param array $options
+ * @return bool
+ */
+	public function beforeDelete($options = array()) {
 		$children = $this->children();
-		$category_ids = Hash::extract($children, '{n}.Category.id');
-		$category_ids[] = $this->id;
+		$categoryIds = Hash::extract($children, '{n}.Category.id');
+		$categoryIds[] = $this->id;
 
-		$pages = $this->WikiPage->findAllByCategoryId($category_ids);
+		$pages = $this->WikiPage->findAllByCategoryId($categoryIds);
 
 		// 自身を含む下位カテゴリのページの所属カテゴリをnull(TOP)へ変更する
 		foreach($pages as $page){
