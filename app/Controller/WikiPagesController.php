6e9c5e65882f86d4ba7af2ec0774dd494c467b7e
hoo89 <hoo89@me.com>
2016-05-25 08:26:30 +0900

    Refactor WikiPagesController

--- app/Controller/WikiPagesController.php
+++ app/Controller/WikiPagesController.php
@@ -13,14 +13,33 @@
 App::uses('AppController', 'Controller');
 
 class WikiPagesController extends AppController {
-	public $components = array('Search.Prg','RequestHandler','Security' => array('validatePost' => false));
 
-	// for Search Plugin
+/**
+ * Using components
+ *
+ * @var array
+ */
+	public $components = array('Search.Prg', 'RequestHandler', 'Security' => array('validatePost' => false));
+
+/**
+ * For Search plugin
+ *
+ * @var array
+ */
 	public $presetVars = true;
 
+/**
+ * helper
+ *
+ * @var array
+ */
 	public $helpers = array('Text');
 
-	// WikiPage order
+/**
+ * Pagenation conditions
+ *
+ * @var array
+ */
 	var $paginate = array(
 		'limit' => 20,
 		'order' => array(
@@ -28,11 +47,11 @@ class WikiPagesController extends AppController {
 		)
 	);
 
-	/**
-	 * Called before Action.
-	 *
-	 * @return void
-	 */
+/**
+ * Called before Action.
+ *
+ * @return void
+ */
 	public function beforeFilter() {
 		parent::beforeFilter();
 		$this->Security->blackHoleCallback = 'blackhole';
@@ -48,15 +67,15 @@ class WikiPagesController extends AppController {
 		}
 	}
 
-	/**
-	 * It makes WikiPage index.
-	 * If it called from requestAction(), this return array of WikiPages.
-	 *
-	 * @return array|void
-	 */
+/**
+ * It makes WikiPage index.
+ * If it called from requestAction(), this return array of WikiPages.
+ *
+ * @return array|void
+ */
 	public function index() {
-		$categoryList = $this->WikiPage->Category->generateTreeList(null,null,null, '-');
-		$this->set('categoryList',$categoryList);
+		$categoryList = $this->WikiPage->Category->generateTreeList(null, null, null, '-');
+		$this->set('categoryList', $categoryList);
 
 		$wikiPages = $this->paginate();
 		if ($this->request->is('requested')) {
@@ -66,15 +85,15 @@ class WikiPagesController extends AppController {
 		}
 	}
 
-	/**
-	 * It makes public WikiPage index for users not logged in.
-	 * If it called from requestAction(), this return array of WikiPages.
-	 *
-	 * @return array|void
-	 */
+/**
+ * It makes public WikiPage index for users not logged in.
+ * If it called from requestAction(), this return array of WikiPages.
+ *
+ * @return array|void
+ */
 	public function public_index() {
 		if($this->Auth->loggedIn()){
-			return $this->redirect(array('action'=>'index'));
+			return $this->redirect(array('action' => 'index'));
 		}
 
 		$this->paginate['conditions'] = array('WikiPage.is_public' => true);
@@ -88,16 +107,16 @@ class WikiPagesController extends AppController {
 		}
 	}
 
-	/**
-	 * Search action.
-	 * If it called from requestAction(), this return array of WikiPages.
-	 *
-	 * @return array|void
-	 */
-	public function find(){
-		$this->set('title','検索結果');
-		$categoryList = $this->WikiPage->Category->generateTreeList(null,null,null, '-');
-		$this->set('categoryList',$categoryList);
+/**
+ * Search action.
+ * If it called from requestAction(), this return array of WikiPages.
+ *
+ * @return array|void
+ */
+	public function find() {
+		$this->set('title', '検索結果');
+		$categoryList = $this->WikiPage->Category->generateTreeList(null, null, null, '-');
+		$this->set('categoryList', $categoryList);
 
 		App::uses('String', 'Utility');
 		$this->Prg->commonProcess();
@@ -112,24 +131,24 @@ class WikiPagesController extends AppController {
 		}
 	}
 
-	/**
-	 * Search action for users not logged in.
-	 * If it called from requestAction(), this return array of WikiPages.
-	 *
-	 * @return array|void
-	 */
-	public function public_find(){
+/**
+ * Search action for users not logged in.
+ * If it called from requestAction(), this return array of WikiPages.
+ *
+ * @return array|void
+ */
+	public function public_find() {
 		if($this->Auth->loggedIn()){
-			$this->redirect(array('action'=>'find'));
+			$this->redirect(array('action' => 'find'));
 		}
 
-		$this->set('title','検索結果');
+		$this->set('title', '検索結果');
 
 		App::uses('String', 'Utility');
 		$this->Prg->commonProcess();
 		$this->passedArgs['is_public'] = true;
 		$this->paginate['conditions'] = $this->WikiPage->parseCriteria($this->passedArgs);
-		$this->set('searchword',$this->request->named);
+		$this->set('searchword', $this->request->named);
 		$wikiPages = $this->paginate();
 		$this->set('wikiPages', $wikiPages);
 		if ($this->request->is('requested')) {
@@ -140,34 +159,34 @@ class WikiPagesController extends AppController {
 		}
 	}
 
-	/**
-	 * Change multiple pages category at once.
-	 *
-	 * - Example Post data
-	 * {'WikiPage'=>{'id'=>{1=>true, 2=>true, 3=>false}, 'category_id'=> 1}
-	 * Change 1 and 2 page's category_id to 1
-	 *
-	 * @return void
-	 */
-	function changePagesCategory(){
+/**
+ * Change multiple pages category at once.
+ *
+ * - Example Post data
+ * {'WikiPage'=>{'id'=>{1=>true, 2=>true, 3=>false}, 'category_id'=> 1}
+ * Change 1 and 2 page's category_id to 1
+ *
+ * @return void
+ */
+	function changePagesCategory() {
 		if ($this->request->is('post')) {
 			foreach ($this->request->data['WikiPage']['id'] as $id => $selected) {
 				if ($selected){
-					$this->WikiPage->save(array('id'=>$id,'category_id'=>$this->request->data['WikiPage']['category_id'],'modified'=>false));
+					$this->WikiPage->save(array('id' => $id, 'category_id' => $this->request->data['WikiPage']['category_id'], 'modified' => false));
 				}
 			}
-			$this->setFlash('カテゴリーを変更しました','alert/success');
-			$this->redirect($this->referer(null,true));
+			$this->setFlash('カテゴリーを変更しました', 'alert/success');
+			$this->redirect($this->referer(null, true));
 		}
 	}
 
-	/**
-	 * WikiPage View
-	 *
-	 * @param string $title
-	 * @return void
-	 * @throws NotFoundException
-	 */
+/**
+ * WikiPage View
+ *
+ * @param string $title
+ * @return void
+ * @throws NotFoundException
+ */
 	public function view($title = null) {
 		$this->set('content_title', $title);
 		$this->helpers[] = 'WikitccParse';
@@ -182,47 +201,47 @@ class WikiPagesController extends AppController {
 
 		if(!$this->Auth->loggedIn() && !$post['WikiPage']['is_public']){
 			$this->Session->write('Auth.redirect', Router::url(null, true));
-			$this->Session->setFlash('そのページを表示するにはログインが必要です','alert/danger');
-			$this->redirect(array('controller'=>'users', 'action'=>'login'));
+			$this->Session->setFlash('そのページを表示するにはログインが必要です', 'alert/danger');
+			$this->redirect(array('controller' => 'users', 'action' => 'login'));
 		}
 
 		$this->set('parents', $this->WikiPage->Category->getPath($post['Category']['id']));
 		$this->set('data', $post);
 	}
 
-	/**
-	 * Add WikiPage
-	 * If posted WikiPage, create page and redirect created one.
-	 *
-	 * @return void
-	 */
+/**
+ * Add WikiPage
+ * If posted WikiPage, create page and redirect created one.
+ *
+ * @return void
+ */
 	public function add() {
-		$categoryList = $this->WikiPage->Category->generateTreeList(null,null,null, '-');
-		$this->set('categoryList',$categoryList);
+		$categoryList = $this->WikiPage->Category->generateTreeList(null, null, null, '-');
+		$this->set('categoryList', $categoryList);
 		$this->set('content_title', null);
 
 		if ($this->request->is('post')) {
 			$this->WikiPage->create();
 			if ($this->WikiPage->save($this->request->data)) {
-				$this->Session->setFlash('ページが作成されました','alert/success');
-				return $this->redirect(array('action' => 'view',$this->request->data['WikiPage']['title']));
+				$this->Session->setFlash('ページが作成されました', 'alert/success');
+				return $this->redirect(array('action' => 'view', $this->request->data['WikiPage']['title']));
 			}
-			$this->Session->setFlash('ページを作成できませんでした','alert/danger');
+			$this->Session->setFlash('ページを作成できませんでした', 'alert/danger');
 		}
 	}
 
-	/**
-	 * Edit WikiPage
-	 * If posted WikiPage, update page and redirect updated one.
-	 *
-	 * @param string $title
-	 * @return void
-	 * @throws NotFoundException
-	 */
+/**
+ * Edit WikiPage
+ * If posted WikiPage, update page and redirect updated one.
+ *
+ * @param string $title
+ * @return void
+ * @throws NotFoundException
+ */
 	public function edit($title = null) {
-		$categoryList = $this->WikiPage->Category->generateTreeList(null,null,null, '-');
+		$categoryList = $this->WikiPage->Category->generateTreeList(null, null, null, '-');
 		$this->set('content_title', $title);
-		$this->set('categoryList',$categoryList);
+		$this->set('categoryList', $categoryList);
 
 		$post = $this->WikiPage->findByTitle($title);
 		if (!$post) {
@@ -234,10 +253,10 @@ class WikiPagesController extends AppController {
 			$this->WikiPage->id = $post['WikiPage']['id'];
 
 			if ($this->WikiPage->save($this->request->data)) {
-				$this->Session->setFlash('ページが更新されました','alert/success');
-				return $this->redirect(array('action' => 'view',$post['WikiPage']['title']));
+				$this->Session->setFlash('ページが更新されました', 'alert/success');
+				return $this->redirect(array('action' => 'view', $post['WikiPage']['title']));
 			}
-			$this->Session->setFlash('ページを更新できませんでした','alert/danger');
+			$this->Session->setFlash('ページを更新できませんでした', 'alert/danger');
 		}
 
 		//for setting form default value
@@ -247,13 +266,13 @@ class WikiPagesController extends AppController {
 		$this->render('add');
 	}
 
-	/**
-	 * Delete WikiPage
-	 *
-	 * @param string $title
-	 * @return void
-	 * @throws NotFoundException MethodNotAllowedException
-	 */
+/**
+ * Delete WikiPage
+ *
+ * @param string $title
+ * @return void
+ * @throws NotFoundException MethodNotAllowedException
+ */
 	public function delete($title = null) {
 		if ($this->request->is('get')) {
 			throw new MethodNotAllowedException();
@@ -263,55 +282,80 @@ class WikiPagesController extends AppController {
 		if (!$post){
 			throw new NotFoundException('ページが見つかりません');
 		}
-		$id=$post['WikiPage']['id'];
+		$id = $post['WikiPage']['id'];
 
 		if ($this->WikiPage->delete($id)) {
-			$this->Session->setFlash('ページ： '.h($title).'を削除しました','alert/success');
+			$this->Session->setFlash('ページ： ' . h($title) . 'を削除しました', 'alert/success');
 			return $this->redirect(array('action' => 'index'));
 		}
 	}
 
-	public function revisions($title=null){
+/**
+ * Show revisions of WikiPage
+ *
+ * @var string $title
+ */
+	public function revisions($title = null) {
 		$post = $this->WikiPage->findByTitle($title);
 		if (!$post){
 			throw new NotFoundException('ページが見つかりません');
 		}
 		$this->WikiPage->id = $post['WikiPage']['id'];
-		$revisions = $this->paginate($this->WikiPage->getRevision(), array('id'=>$this->WikiPage->id));
-		$this->set('wikiPages',$revisions);
-		$this->set('content_title',$post['WikiPage']['title']);
+		$revisions = $this->paginate($this->WikiPage->getRevision(), array('id' => $this->WikiPage->id));
+		$this->set('wikiPages', $revisions);
+		$this->set('content_title', $post['WikiPage']['title']);
 	}
 
-	public function view_revision($version_id){
-		$post = $this->WikiPage->getRevision()->findByVersionId($version_id);
-		$this->set('post',$post);
+/**
+ * View body of WikiPagesRev
+ *
+ * @var string $versionId wiki_pages_revs.version_id
+ */
+	public function view_revision($versionId) {
+		$post = $this->WikiPage->getRevision()->findByVersionId($versionId);
+		$this->set('post', $post);
 	}
 
-	public function view_prev_diff($version_id){
-		$old = $this->WikiPage->getRevision()->findByVersionId($version_id);
+/**
+ * View diff between a page designated by versionId and the previous page
+ *
+ * @var string $versionId wiki_pages_revs.version_id
+ */
+	public function view_prev_diff($versionId) {
+		$old = $this->WikiPage->getRevision()->findByVersionId($versionId);
 		$this->WikiPage->id = $old['WikiPage']['id'];
-		$diff = $this->WikiPage->diff(null, $version_id, array('limit'=>2));
-		$this->set('diff',$diff);
+		$diff = $this->WikiPage->diff(null, $versionId, array('limit' => 2));
+		$this->set('diff', $diff);
 	}
 
-	public function view_latest_diff($version_id){
-		$old = $this->WikiPage->getRevision()->findByVersionId($version_id);
+/**
+ * View diff between a page designated by versionId and the latest page
+ *
+ * @var string $versionId wiki_pages_revs.version_id
+ */
+	public function view_latest_diff($versionId) {
+		$old = $this->WikiPage->getRevision()->findByVersionId($versionId);
 		$current = $this->WikiPage->findById($old['WikiPage']['id']);
 		$diff = array_merge_recursive($current, $old);
-		$this->set('diff',$diff);
+		$this->set('diff', $diff);
 		$this->render('view_prev_diff');
 	}
 
-	public function preview(){
+/**
+ * If request by Ajax, return rendered page for preview.
+ */
+	public function preview() {
 		if ($this->RequestHandler->isAjax()) {
-			$this->set('body',$this->request->data['body']);
+			$this->set('body', $this->request->data['body']);
 		}
 	}
 
-	/**
-	 * blackhole method for SecurityComponent
-	 */
-	public function blackhole($type){
+/**
+ * blackhole method for SecurityComponent
+ *
+ * @param string $type
+ */
+	public function blackhole($type) {
 		switch($type){
 			case 'csrf' :
 				$this->Session->setFlash('送信できませんでした.セッションがタイムアウトした可能性があります.もう一度再読み込みしてやり直してください',
@@ -324,7 +368,11 @@ class WikiPagesController extends AppController {
 		}
 	}
 
-	public function add_comment(){
+/**
+ * Add comment
+ * This method edits the WikiPage directly.
+ */
+	public function add_comment() {
 		if ($this->request->is('post')) {
 			$post = $this->WikiPage->findById($this->request->data['WikiPage']['id']);
 			if (!$post) {
@@ -333,19 +381,19 @@ class WikiPagesController extends AppController {
 			$this->WikiPage->id = $post['WikiPage']['id'];
 			$content = $post['WikiPage']['body'];
 			$name = h($this->request->data['WikiPage']['name']);
-			$message = ereg_replace("\n|\r",'',nl2br(h($this->request->data['WikiPage']['comment']), false));
+			$message = ereg_replace("\n|\r", '', nl2br(h($this->request->data['WikiPage']['comment']), false));
 			if (empty($name)){
 				$name = "名無しさん";
 			}
-			$wiki_comment = "*$message - $name\n";
-			$content = str_replace('{{comment}}', $wiki_comment.'{{comment}}', $content);
+			$wikiComment = "*$message - $name\n";
+			$content = str_replace('{{comment}}', $wikiComment . '{{comment}}', $content);
 			$post['WikiPage']['body'] = $content;
 
 			if ($this->WikiPage->save($post)) {
-				$this->Session->setFlash('ページが更新されました','alert/success');
-				return $this->redirect(array('action' => 'view',$post['WikiPage']['title']));
+				$this->Session->setFlash('ページが更新されました', 'alert/success');
+				return $this->redirect(array('action' => 'view', $post['WikiPage']['title']));
 			}else{
-				$this->Session->setFlash('ページを更新できませんでした','alert/danger');
+				$this->Session->setFlash('ページを更新できませんでした', 'alert/danger');
 			}
 		}
 	}
